56c890d6d1dd7975cf2f31b0bf49ed00cba19f71
JohnnytheShark <orellanajonathan7@gmail.com>
2026-08-25 16:39:18 -0400

    Add hardtime.nvim plugin

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      -- You can customize the settings here. 
+      -- By default, it is enabled globally.
+      disable_mouse = false, -- We'll leave the mouse enabled while you are learning
+    },
+  },
+}
